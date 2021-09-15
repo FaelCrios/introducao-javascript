914c251b7ab50529af1a799c01ad0ce560a925fa
Rafael Colin <faelnek@gmail.com>
2021-09-15 13:49:56 -0300

    Remover pacientes

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,9 +2,17 @@ var pacientes = document.querySelectorAll('.paciente')
 
 var tabela = document.querySelector('table')
 
-pacientes.forEach(function (paciente) {
-  paciente.addEventListener('dblclick', function () {
-    console.log('fui clicado dblclick')
-    this.remove()
-  })
+tabela.addEventListener('dblclick', function (event) {
+  event.target.parentNode.classList.add('fadeOut')
+
+  setTimeout(function () {
+    event.target.parentNode.remove()
+  }, 300)
 })
+
+// pacientes.forEach(function (paciente) {
+//   paciente.addEventListener('dblclick', function () {
+//     console.log('fui clicado dblclick')
+//     this.remove()
+//   })
+// })
